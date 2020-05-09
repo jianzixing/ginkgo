d8fd3ab7333dc5608bf05f0083642315b01af13a
yangankang <yak1992@foxmail.com>
2020-05-09 10:04:32 +0800

    提交

--- lib/Ginkgo.ts
+++ lib/Ginkgo.ts
@@ -257,4 +257,6 @@ export default class Ginkgo {
     }
 }
 
-console.log("%c%c北京简子行科技有限公司%c https://www.jianzixing.com.cn", "line-height:28px;", "line-height:28px;padding:4px 9px 4px 9px;background:#FF7F50;color:#fff;font-size:16px;margin-right:15px;border-radius:6px", "color:#333333;line-height:28px;font-size:16px;");
+console.log("%c%c北京简子行科技有限公司%c https://www.jianzixing.com.cn", "line-height:20px;",
+    "line-height:20px;padding:4px 9px 4px 9px;background:#0B61A4;color:#fff;font-size:13px;margin-right:15px;border-radius:6px",
+    "color:#333333;line-height:20px;font-size:13px;");
